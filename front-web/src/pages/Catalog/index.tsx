70bb1035313373ec901124147d1d51231d3eacd2
rbarbosa1985 <betoanselmo@gmail.com>
2020-11-06 00:07:25 -0300

    Pagination

--- front-web/src/pages/Catalog/index.tsx
+++ front-web/src/pages/Catalog/index.tsx
@@ -5,21 +5,23 @@ import makeRequest from 'core/utils/request';
 import ProductCard from './components/ProductCard';
 import ProductCardLoader from './components/Loaders/ProductCardLoader';
 import './styles.scss';
+import Pagination from 'core/components/Pagination';
 
 const Catalog = () => {
 
      const [productsResponse, setProductsResponse] = useState<ProductResponse>();
      const [isLoading, setIsLoading] = useState(false);
+     const [activePage, setActivePage] = useState(0);
 
      useEffect(()=> {
           const params = {
-               page : 0,
-               linesPerPage: 12
+               page : activePage,
+               linesPerPage: 18
           }
           setIsLoading(true);
           makeRequest({ url: '/products' ,  params}).then(response => setProductsResponse(response.data))
                .finally(() => setIsLoading(false));
-     }, [])
+     }, [activePage])
 
      return (
           <div className="catalog-container">
@@ -33,6 +35,7 @@ const Catalog = () => {
                          ))
                     )}
                </div>
+               {productsResponse && <Pagination totalPages={productsResponse.totalPages} activePage={activePage} onChange={page => setActivePage(page)}/>}
           </div>
      )
 }
