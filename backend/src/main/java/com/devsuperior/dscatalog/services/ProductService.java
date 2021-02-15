9e0cf2a84b3de8e97877149ee883f9513dd5d532
Roberto Barbosa <betoanselmo@gmail.com>
2021-02-15 14:41:50 -0300

    N+1 Categories

--- backend/src/main/java/com/devsuperior/dscatalog/services/ProductService.java
+++ backend/src/main/java/com/devsuperior/dscatalog/services/ProductService.java
@@ -85,6 +85,7 @@ public class ProductService {
 	public Page<ProductDTO> findAllPaged(PageRequest pageRequest, Long categoryId, String name) {
 		List<Category> categories = (categoryId == 0) ? null : Arrays.asList(categoryRepository.getOne(categoryId));
 		Page<Product> list = repository.find(categories, name,pageRequest);
+		repository.find(list.toList());
 		return list.map(x -> new ProductDTO(x, x.getCategories()));
 	}
 	
